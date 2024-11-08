f656e348cb13097f455ee2c7830b860f5953749d
semi-cloud <sksmsrkdtpal@naver.com>
2024-11-08 23:11:15 +0900

    :bug: 디벨롭 브랜치 변경 사항 반영

--- ahachul_backend/core-module/src/main/kotlin/backend/team/ahachul_backend/common/dto/GoogleUserInfoDto.kt
+++ ahachul_backend/core-module/src/main/kotlin/backend/team/ahachul_backend/common/dto/GoogleUserInfoDto.kt
@@ -1,7 +1,9 @@
 package backend.team.ahachul_backend.common.dto
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class GoogleUserInfoDto(
     @JsonProperty("id") val id: String,
     @JsonProperty("email") val email: String,
