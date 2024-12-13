e8a495ccc7263e7f965c1e1cca6a757c2109101d
discphy <dinophy@nate.com>
2024-12-13 14:37:49 +0900

    :recycle: import문 최적화 (#295)

--- core/src/main/kotlin/backend/team/ahachul_backend/api/lost/application/command/in/UpdateLostPostStatusCommand.kt
+++ core/src/main/kotlin/backend/team/ahachul_backend/api/lost/application/command/in/UpdateLostPostStatusCommand.kt
@@ -1,7 +1,6 @@
 package backend.team.ahachul_backend.api.lost.application.service.command.`in`
 
 import backend.team.ahachul_backend.api.lost.domain.model.LostStatus
-import org.springframework.web.multipart.MultipartFile
 
 class UpdateLostPostStatusCommand (
     val id: Long,
