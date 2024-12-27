98e22238b9a02bdd06c9b70d18c7c3c8ff95df44
discphy <dinophy@nate.com>
2024-12-27 23:32:29 +0900

    :recycle: application 댓글 조회 Controller import문 최적화 (#303)

--- application/src/main/kotlin/backend/team/ahachul_backend/api/lost/adapter/web/in/LostPostCommentController.kt
+++ application/src/main/kotlin/backend/team/ahachul_backend/api/lost/adapter/web/in/LostPostCommentController.kt
@@ -2,7 +2,6 @@ package backend.team.ahachul_backend.api.lost.adapter.web.`in`
 
 import backend.team.ahachul_backend.api.comment.adapter.web.`in`.dto.CreateCommentDto
 import backend.team.ahachul_backend.api.comment.adapter.web.`in`.dto.GetCommentsDto
-import backend.team.ahachul_backend.api.comment.application.command.GetCommentsCommand
 import backend.team.ahachul_backend.api.comment.application.port.`in`.CommentUseCase
 import backend.team.ahachul_backend.api.comment.domain.model.PostType
 import backend.team.ahachul_backend.common.annotation.Authentication
