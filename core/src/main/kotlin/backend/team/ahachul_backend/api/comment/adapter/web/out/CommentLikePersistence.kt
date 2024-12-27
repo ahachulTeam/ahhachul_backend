1ee154ba0adbf3520c5d733a1063369aae47dfe0
discphy <dinophy@nate.com>
2024-12-27 23:20:05 +0900

    :recycle: 댓글 좋아요 미사용 메서드 제거 (#303)

--- core/src/main/kotlin/backend/team/ahachul_backend/api/comment/adapter/web/out/CommentLikePersistence.kt
+++ core/src/main/kotlin/backend/team/ahachul_backend/api/comment/adapter/web/out/CommentLikePersistence.kt
@@ -3,7 +3,6 @@ package backend.team.ahachul_backend.api.comment.adapter.web.out
 import backend.team.ahachul_backend.api.comment.application.port.out.CommentLikeReader
 import backend.team.ahachul_backend.api.comment.application.port.out.CommentLikeWriter
 import backend.team.ahachul_backend.api.comment.domain.entity.CommentLikeEntity
-import backend.team.ahachul_backend.common.domain.model.YNType
 import org.springframework.stereotype.Component
 
 @Component
@@ -22,13 +21,4 @@ class CommentLikePersistence(
     override fun find(commentId: Long, memberId: Long): CommentLikeEntity? {
         return repository.findByCommentIdAndMemberId(commentId, memberId)
     }
-
-    override fun exist(commentId: Long, memberId: Long): Boolean {
-        return repository.existsByCommentIdAndMemberId(commentId, memberId)
-    }
-
-    override fun count(commentId: Long, ynType: YNType): Int {
-        return repository.countByCommentIdAndLikeYn(commentId, ynType)
-    }
-
 }
--- core/src/main/kotlin/backend/team/ahachul_backend/api/comment/application/port/out/CommentLikeReader.kt
+++ core/src/main/kotlin/backend/team/ahachul_backend/api/comment/application/port/out/CommentLikeReader.kt
@@ -1,13 +1,8 @@
 package backend.team.ahachul_backend.api.comment.application.port.out
 
 import backend.team.ahachul_backend.api.comment.domain.entity.CommentLikeEntity
-import backend.team.ahachul_backend.common.domain.model.YNType
 
 interface CommentLikeReader {
 
     fun find(commentId: Long, memberId: Long): CommentLikeEntity?
-
-    fun exist(commentId: Long, memberId: Long): Boolean
-
-    fun count(commentId: Long, ynType: YNType): Int
 }
