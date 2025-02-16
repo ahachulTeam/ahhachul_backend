1452248d49f84b7654ae0b858881ee49e39d5c9a
semi-cloud <sksmsrkdtpal@naver.com>
2025-02-16 18:21:05 +0900

    :bug: all.json 파일 경로 수정 (#305)

--- core/src/main/kotlin/backend/team/ahachul_backend/common/constant/CommonConstant.kt
+++ core/src/main/kotlin/backend/team/ahachul_backend/common/constant/CommonConstant.kt
@@ -6,7 +6,7 @@ class CommonConstant {
     companion object {
         // HASHTAG
         const val HASHTAG_REDIS_KEY = "hashtag_rank"
-        const val LOST_FILE_URL = "/home/all.json"
+        const val LOST_FILE_URL = "/app/ahachul_data/datas/all.json"
         const val HASHTAG_FILE_URL = "logs/archive/hashtag/hashtag_log.log"
         val HASHTAG_LOG_DATETIME_FORMATTER: DateTimeFormatter = DateTimeFormatter.ofPattern("yy-MM-dd HH:mm:ss")
         val CURSOR_PAGING_DATETIME_FORMATTER: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
