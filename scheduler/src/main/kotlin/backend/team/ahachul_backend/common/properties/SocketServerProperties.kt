5ade3c389aa382bdff12ecf3ce4c9ac15afa0b07
semi-cloud <sksmsrkdtpal@naver.com>
2025-02-16 14:08:56 +0900

    :bug: logback xml config path 수정 (#254)

--- scheduler/src/main/kotlin/backend/team/ahachul_backend/common/properties/SocketServerProperties.kt
+++ scheduler/src/main/kotlin/backend/team/ahachul_backend/common/properties/SocketServerProperties.kt
@@ -2,10 +2,15 @@ package backend.team.ahachul_backend.common.properties
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
 
 @Configuration
 @ConfigurationProperties(prefix = "socket-server")
 data class SocketServerProperties(
     var port: String = "",
     var configPath: String = ""
-)
+) {
+    fun getConfigPath(): String {
+        return ClassPathResource(configPath).file.absolutePath
+    }
+}
