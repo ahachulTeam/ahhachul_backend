b20a03b8fccbd243555e0596997efbb4d1809ed3
semi-cloud <sksmsrkdtpal@naver.com>
2025-02-28 21:26:04 +0900

    :bug: 오타 수정

--- application/src/test/kotlin/backend/team/ahachul_backend/api/complaint/adapter/web/in/ComplaintPostControllerDocsTest.kt
+++ application/src/test/kotlin/backend/team/ahachul_backend/api/complaint/adapter/web/in/ComplaintPostControllerDocsTest.kt
@@ -80,7 +80,7 @@ class ComplaintPostControllerDocsTest : CommonDocsTestConfig() {
                     getDocsRequest(),
                     getDocsResponse(),
                     queryParameters(
-                        parameterWithName("subwayLineId2").description("민원 호선 리스트").optional(),
+                        parameterWithName("subwayLineIds").description("민원 호선 리스트").optional(),
                         parameterWithName("keyword").description("검색 키워드 명칭").optional(),
                         parameterWithName("pageToken").description("base64로 인코딩 된 페이지 토큰 문자열").optional(),
                         parameterWithName("pageSize").description("페이지 노출 데이터 수. index 0부터 시작"),
