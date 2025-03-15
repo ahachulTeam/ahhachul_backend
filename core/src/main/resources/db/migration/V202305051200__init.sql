1e8d4345b062a2ae35d3bed96bab07c947c7bf29
Hwang Gyuyeon <hwgyun@gmail.com>
2025-03-15 14:53:52 +0900

    flyway 파일 분리

--- core/src/main/resources/db/migration/V202305051200__init.sql
+++ core/src/main/resources/db/migration/V202305051200__init.sql
@@ -72,18 +72,6 @@ CREATE TABLE tb_member
     PRIMARY KEY (member_id)
 );
 
-CREATE TABLE tb_fcm_token
-(
-    token_id         BIGINT auto_increment NOT NULL,
-    member_id        REFERENCES tb_member(member_id) NOT NULL,
-    token            VARCHAR(255) NOT NULL,
-    created_at       TIMESTAMP NOT NULL,
-    created_by       VARCHAR(50) NOT NULL,
-    updated_at       TIMESTAMP NOT NULL,
-    updated_by       VARCHAR(50) NOT NULL,
-    PRIMARY KEY (token_id)
-);
-
 CREATE TABLE tb_lost_post
 (
     lost_post_id   BIGINT NOT NULL auto_increment,
