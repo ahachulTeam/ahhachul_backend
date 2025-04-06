65abb4eaca8635c1cb6ff410fbc99ceb7fedb7dc
semi-cloud <sksmsrkdtpal@naver.com>
2025-04-06 23:37:52 +0900

    :bug: create ddl문 문법 오류 수정

--- core/src/main/resources/db/migration/V202503151400__add_fcm_token.sql
+++ core/src/main/resources/db/migration/V202503151400__add_fcm_token.sql
@@ -1,11 +1,11 @@
 CREATE TABLE tb_fcm_token
 (
-    token_id         BIGINT auto_increment NOT NULL,
-    member_id        REFERENCES tb_member(member_id) NOT NULL,
-    token            VARCHAR(255) NOT NULL,
-    created_at       TIMESTAMP NOT NULL,
-    created_by       VARCHAR(50) NOT NULL,
-    updated_at       TIMESTAMP NOT NULL,
-    updated_by       VARCHAR(50) NOT NULL,
+    token_id    BIGINT AUTO_INCREMENT NOT NULL,
+    member_id   BIGINT NOT NULL,
+    token       VARCHAR(255) NOT NULL,
+    created_at  TIMESTAMP NOT NULL,
+    created_by  VARCHAR(50) NOT NULL,
+    updated_at  TIMESTAMP NOT NULL,
+    updated_by  VARCHAR(50) NOT NULL,
     PRIMARY KEY (token_id)
 );
