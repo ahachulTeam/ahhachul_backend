2127999f636bb845f91f83fecffa7cdd751744e7
semi-cloud <sksmsrkdtpal@naver.com>
2026-03-05 23:40:49 +0900

    :sparkles: 실시간 열차 혼잡도 정보 조회 lock 추가 (#376)

--- application/src/main/kotlin/backend/team/ahachul_backend/api/train/application/service/TrainService.kt
+++ application/src/main/kotlin/backend/team/ahachul_backend/api/train/application/service/TrainService.kt
@@ -103,7 +103,7 @@ class TrainService(
                 return it
             }
 
-            logger.info("[cache miss] 외부 API 호출 시작 (분산 락 획득): lockKey=$lockKey")
+            logger.info("[cache miss] 외부 열차 도착 정보 API 호출 시작 (분산 락 획득): lockKey=$lockKey")
             val result = requestTrainRealTimesAndSorting(station.name)
             result.forEach { (key, value) ->
                 trainCacheUtils.setCache(key.toLong(), stationId, value)
@@ -113,14 +113,13 @@ class TrainService(
             return upDownType?.let { type ->
                 trainRealTimes.filter { it.upDownType == type }.take(4)
             } ?: trainRealTimes
-
         } catch (e: InterruptedException) {
             logger.error("Lock 획득 중 인터럽트 발생", e)
             throw BusinessException(ResponseCode.LOCK_ACQUISITION_FAILED)
         } finally {
             if (lock.isHeldByCurrentThread) {
                 lock.unlock()
-                logger.info("[cache miss] 외부 API 호출 완료 (분산 락 해제): lockKey=$lockKey")
+                logger.info("[cache miss] 외부 열차 도착 정보 API 호출 완료 (분산 락 해제): lockKey=$lockKey")
             }
         }
     }
@@ -195,23 +194,54 @@ class TrainService(
     }
 
     /**
-     * 실시간 열차 혼잡도 정보를 조회하는 메서드
+     * 실시간 열차 혼잡도 정보를 조회하는 메서드 (Redis 분산 락 적용)
      */
     @CircuitBreaker(name = CUSTOM_CIRCUIT_BREAKER, fallbackMethod = "fallbackOnExternalCongestionApiGet")
     override fun getTrainCongestion(command: GetCongestionCommand): GetCongestionDto.Response {
         val subwayLineId = subwayLineReader.getById(command.subwayLineId).id
         val trainNo = command.trainNo
 
-        congestionCacheUtils.getCache(subwayLineId, trainNo)?.let { return it }
+        congestionCacheUtils.getCache(subwayLineId, trainNo)?.let {
+            logger.info("[cache hit] 혼잡도 응답 반환: subwayLineId=$subwayLineId, trainNo=$trainNo")
+            return it
+        }
 
-        val correctTrainNum = getCorrectTrainNum(subwayLineId, trainNo)
-        val response = trainCongestionClient.getCongestions(subwayLineId, correctTrainNum.toInt())
-        val trainCongestion = response.data!!
+        val lockKey = "congestion:$subwayLineId-$trainNo"
+        val lock = redissonClient.getLock(lockKey)
 
-        val congestions = mapCongestionDto(response.success, trainCongestion)
-        val congestionDto = GetCongestionDto.Response.from(correctTrainNum, congestions)
-        congestionCacheUtils.setCache(subwayLineId, correctTrainNum, congestionDto)
-        return congestionDto
+        try {
+            val acquired = lock.tryLock(10, 15, TimeUnit.SECONDS)
+            if (!acquired) {
+                logger.error("분산 락 획득 실패: lockKey=$lockKey")
+                throw BusinessException(ResponseCode.LOCK_ACQUISITION_FAILED)
+            }
+
+            congestionCacheUtils.getCache(subwayLineId, trainNo)?.let {
+                logger.info("[cache miss] 이미 캐싱된 데이터로 인해 바로 락 해제 후 종료: lockKey=$lockKey")
+                lock.unlock()
+                return it
+            }
+
+            logger.info("[cache miss] 외부 열차 혼잡도 API 호출 시작 (분산 락 획득): lockKey=$lockKey")
+
+            val correctTrainNum = getCorrectTrainNum(subwayLineId, trainNo)
+            val response = trainCongestionClient.getCongestions(subwayLineId, correctTrainNum.toInt())
+            val trainCongestion = response.data!!
+
+            val congestions = mapCongestionDto(response.success, trainCongestion)
+            val congestionDto = GetCongestionDto.Response.from(correctTrainNum, congestions)
+
+            congestionCacheUtils.setCache(subwayLineId, correctTrainNum, congestionDto)
+            return congestionDto
+        } catch (e: InterruptedException) {
+            logger.error("Lock 획득 중 인터럽트 발생", e)
+            throw BusinessException(ResponseCode.LOCK_ACQUISITION_FAILED)
+        } finally {
+            if (lock.isHeldByCurrentThread) {
+                lock.unlock()
+                logger.info("[cache miss] 외부 열차 혼잡도 API 처리 완료 (분산 락 해제): lockKey=$lockKey")
+            }
+        }
     }
 
     private fun getCorrectTrainNum(subwayLineId: Long, trainNo: String): String {
